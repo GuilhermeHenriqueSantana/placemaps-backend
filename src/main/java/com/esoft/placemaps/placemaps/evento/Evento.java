9a3b40a21f7d3726d823b1caaeadc00ac217bb86
Keven Luan da Silva <ra-19138376-2@alunos.unicesumar.edu.br>
2021-08-23 22:21:35 -0300

    Criando entidade evento e seus relacionamtos

---
+++ src/main/java/com/esoft/placemaps/placemaps/evento/Evento.java
@@ -0,0 +1,44 @@
+package com.esoft.placemaps.placemaps.evento;
+
+import com.esoft.placemaps.configuration.basicclass.BasicClass;
+import com.esoft.placemaps.placemaps.localizacao.Localizacao;
+import com.esoft.placemaps.placemaps.ponto.Ponto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Evento extends BasicClass {
+
+    @Column(name = "inicio", nullable = false)
+    private Date inicio;
+
+    @Column(name = "fim", nullable = false)
+    private Date fim;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "descricao", nullable = false, length = 1000)
+    private String descricao;
+
+    @OneToOne
+    @JoinColumn(name = "ponto_id")
+    private Ponto ponto;
+
+    @OneToOne
+    @JoinColumn(name = "localizacao_id")
+    private Localizacao localizacao;
+
+}
