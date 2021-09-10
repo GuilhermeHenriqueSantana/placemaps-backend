bf41037c202f9981d44862f90dfc4b745d1d6c54
KevenLuanNSG <kevenluan2012@gmail.com>
2021-09-09 21:24:13 -0300

    Mudança na forma de obter usuario

--- src/main/java/com/esoft/placemaps/placemaps/pedidocadastro/PedidoCadastroRepository.java
+++ src/main/java/com/esoft/placemaps/placemaps/pedidocadastro/PedidoCadastroRepository.java
@@ -2,6 +2,10 @@ package com.esoft.placemaps.placemaps.pedidocadastro;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PedidoCadastroRepository extends JpaRepository<PedidoCadastro, String> {
+
+  Optional<PedidoCadastro> findFirstByEmail(String email);
 
 }
