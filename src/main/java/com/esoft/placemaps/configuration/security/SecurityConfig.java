dc5bc6973abe2b3b88685dd0afd9393ea55585b0
KevenLuanNSG <kevenluan2012@gmail.com>
2021-09-14 21:23:18 -0300

    Status de ativo e inativo no ponto

--- src/main/java/com/esoft/placemaps/configuration/security/SecurityConfig.java
+++ src/main/java/com/esoft/placemaps/configuration/security/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/api/item").hasAnyAuthority("PROPRIETARIO");
         http.authorizeRequests().antMatchers("/api/localizacao").hasAnyAuthority("PROPRIETARIO");
         http.authorizeRequests().antMatchers("/api/ponto/categoria/**").hasAnyAuthority("PROPRIETARIO");
+        http.authorizeRequests().antMatchers("/api/ponto/ativar/**").hasAnyAuthority("PROPRIETARIO");
+        http.authorizeRequests().antMatchers("/api/ponto/desativar/**").hasAnyAuthority("PROPRIETARIO");
         http.authorizeRequests().antMatchers("/api/evento").hasAnyAuthority("PROPRIETARIO");
 
         http.authorizeRequests().antMatchers("/api/avaliacao").hasAnyAuthority("PROPRIETARIO", "USUARIO");
