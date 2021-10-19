02645839d0f683724b524ded2997d78a648112cb
GuilhermeHenriqueSantana <gui.guihenrique@hotmail.com>
2021-10-18 21:56:15 -0300

    16385 atualizar item

--- src/main/java/com/esoft/placemaps/placemaps/item/ItemController.java
+++ src/main/java/com/esoft/placemaps/placemaps/item/ItemController.java
@@ -1,10 +1,13 @@
 package com.esoft.placemaps.placemaps.item;
 
+import com.esoft.placemaps.placemaps.item.dto.ItemAtualizarDTO;
 import com.esoft.placemaps.placemaps.item.dto.ItemFormDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,10 @@ public class ItemController {
     @PostMapping
     public ResponseEntity<Item> salvar(@RequestBody ItemFormDTO itemFormDTO) {
         return ResponseEntity.ok(itemService.salvar(itemFormDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> atualizar(@RequestBody ItemAtualizarDTO itemAtualizarDTO, @PathVariable String id) {
+        return ResponseEntity.ok(itemService.atualizar(id, itemAtualizarDTO));
     }
 }
