48a370ce848b4f8e31843a79b887143a6e50172d
GuilhermeHenriqueSantana <gui.guihenrique@hotmail.com>
2021-11-08 21:56:33 -0300

    CatergoriaController teste

---
+++ src/test/java/com/esoft/placemaps/placemaps/categoria/CategoriaControllerTeste.java
@@ -0,0 +1,59 @@
+package com.esoft.placemaps.placemaps.categoria;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@AutoConfigureMockMvc
+@WithMockUser(roles = "ADMIN")
+@SpringBootTest
+public class CategoriaControllerTeste {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper jacksonObjectMapper;
+
+    @MockBean
+    private CategoriaRepository categoriaRepository;
+
+    @Test
+    void salvar() throws Exception {
+        Categoria categoria = new Categoria("Categoria 1");
+        
+        Mockito.when(this.categoriaRepository.save(categoria))
+                .thenReturn(categoria);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(categoria);
+    
+        String categoriaJson = mockMvc.perform(post("/api/categoria")
+                .content(requestJson))
+                .andReturn()  
+                .getResponse()
+                .getContentAsString();  
+
+        System.out.println("aquiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+        System.out.println(categoriaJson);
+        System.out.println("aquiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+                
+        Categoria categoriaSalva = jacksonObjectMapper.readValue(categoriaJson, Categoria.class);
+
+        assertEquals(categoria, categoriaSalva);
+  }
+}
